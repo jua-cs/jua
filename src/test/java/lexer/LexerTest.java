aa46d2fe418cb5e6cb4833cc2bdc6057982af53a
Rémi Calixte <remicalixte.rmc@gmail.com>
2019-09-24 17:02:17 +0200

    fix test

--- src/test/java/lexer/LexerTest.java
+++ src/test/java/lexer/LexerTest.java
@@ -15,60 +15,50 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LexerTest {
 
-      @Test
-      void testVariableAssignment() {
-        String in = "x = y";
-        Lexer lex = new Lexer(in);
-        Stream<Token> stream =
-            Stream.generate(
-                    () -> {
-                      try {
-                        return lex.nextToken();
-                      } catch (IOException e) {
-                        e.printStackTrace();
-                      }
-                      return null;
-                    })
-                .limit(3);
+  @Test
+  void testVariableAssignment() {
+    String in = "x = y";
+    Lexer lex = new Lexer(in);
+    Stream<Token> stream =
+        Stream.generate(
+                () -> {
+                  return lex.nextToken();
+                })
+            .limit(3);
 
-        ArrayList<Token> list = stream.collect(Collectors.toCollection(ArrayList::new));
+    ArrayList<Token> list = stream.collect(Collectors.toCollection(ArrayList::new));
 
-        ArrayList<Token> expected = new ArrayList<Token>();
-        expected.add(new TokenLiteral(Literal.IDENTIFIER, "x", 0, 0));
-        expected.add(new TokenLiteral(Literal.IDENTIFIER, "y", 0, 4));
+    ArrayList<Token> expected = new ArrayList<Token>();
+    expected.add(new TokenLiteral(Literal.IDENTIFIER, "x", 0, 0));
+    expected.add(new TokenLiteral(Literal.IDENTIFIER, "y", 0, 4));
 
-        assertEquals(expected, list);
-      }
+    assertEquals(expected, list);
+  }
 
-      @Test
-      void testLuaFactorialWithCommentaries() {
-        String in =
-            " -- defines a factorial function\n"
-                + "    function fact (n)\n"
-                + "      if n == 0 then\n"
-                + "        return 1\n"
-                + "      else\n"
-                + "        return n * fact(n-1)\n"
-                + "      end\n"
-                + "    end\n"
-                + "    \n"
-                + "    print(\"enter a number:\")\n"
-                + "    a = io.read(\"*number\")        -- read a number\n"
-                + "    print(fact(a))";
+  @Test
+  void testLuaFactorialWithCommentaries() {
+    String in =
+        " -- defines a factorial function\n"
+            + "    function fact (n)\n"
+            + "      if n == 0 then\n"
+            + "        return 1\n"
+            + "      else\n"
+            + "        return n * fact(n-1)\n"
+            + "      end\n"
+            + "    end\n"
+            + "    \n"
+            + "    print(\"enter a number:\")\n"
+            + "    a = io.read(\"*number\")        -- read a number\n"
+            + "    print(fact(a))";
 
-        Lexer lex = new Lexer(in);
-        Stream<Token> stream =
-            Stream.generate(
-                    () -> {
-                      try {
-                        return lex.nextToken();
-                      } catch (IOException e) {
-                        e.printStackTrace();
-                      }
-                      return null;
-                    })
-                .limit(50);
+    Lexer lex = new Lexer(in);
+    Stream<Token> stream =
+        Stream.generate(
+                () -> {
+                  return lex.nextToken();
+                })
+            .limit(50);
 
-        stream.forEach(System.out::println);
-      }
-    }
+    stream.forEach(System.out::println);
+  }
+}
