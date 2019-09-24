14ff2a595d22e37344d498c2c857ab2d472b4c37
Julen Dixneuf <julen.dixneuf@student.ecp.fr>
2019-09-24 16:13:54 +0200

    fix TokenDelimiter signature

--- src/main/java/token/TokenDelimiter.java
+++ src/main/java/token/TokenDelimiter.java
@@ -3,7 +3,8 @@ package token;
 public class TokenDelimiter extends Token {
   private Delimiter delimiter;
 
-  public TokenDelimiter(int line, int position, Delimiter delimiter) {
+
+  TokenDelimiter(Delimiter delimiter, int line, int position) {
     super(TokenType.DELIMITER, delimiter.toString(), line, position);
     this.delimiter = delimiter;
   }
