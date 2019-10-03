95399213c61b5143029fff2de44c81914a87c22b
Sami Tabet <sami.tabet@datadoghq.com>
2019-10-03 15:59:55 +0200

    evaluator: add lua ==

--- src/main/java/objects/LuaObject.java
+++ src/main/java/objects/LuaObject.java
@@ -3,11 +3,17 @@ package objects;
 import evaluator.IllegalTypeException;
 
 public interface LuaObject {
+  public static boolean areSameType(LuaObject o1, LuaObject o2) throws IllegalTypeException {
+    return o1.getClass() == o2.getClass();
+  }
+
   public static void ensureSameType(LuaObject o1, LuaObject o2) throws IllegalTypeException {
-    throw new IllegalTypeException(
-        String.format(
-            "Expected %s (type %s) and %s (type %s) to have same type",
-            o1, o1.getClass(), o2, o2.getClass()));
+    if (!areSameType(o1, o2)) {
+      throw new IllegalTypeException(
+          String.format(
+              "Expected %s (type %s) and %s (type %s) to have same type",
+              o1, o1.getClass(), o2, o2.getClass()));
+    }
   }
 
   // TODO casting functions
