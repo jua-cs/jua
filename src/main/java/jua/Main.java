5ff774011c35a051d347524935918577a39bbed4
Rémi Calixte <remicalixte.rmc@gmail.com>
2019-10-03 16:27:35 +0200

    revive Main class

---
+++ src/main/java/jua/Main.java
@@ -0,0 +1,30 @@
+package jua;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import jua.lexer.Lexer;
+import jua.parser.IllegalParseException;
+import jua.parser.Parser;
+import jua.token.Token;
+
+public class Main {
+
+  public static void main(String[] args) throws IllegalParseException {
+    String text = null;
+    try {
+      text = new String(System.in.readAllBytes());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    System.out.println("---");
+    System.out.println("Lexing:");
+    ArrayList<Token> tokens = (new Lexer(text)).getNTokens(0);
+    tokens.forEach(System.out::println);
+    System.out.println("---");
+    System.out.println("Parsing:");
+    Parser parser = new Parser(tokens);
+    parser.parse().forEach(System.out::println);
+    System.out.println("---");
+  }
+}
