28db7918689da995b05982447d9ad68b65a1049e
Julen Dixneuf <julen.dixneuf@student.ecp.fr>
2019-10-03 15:47:07 +0200

    implement evaluate in ExpressionNotEqual

--- src/main/java/ast/ExpressionNotEqual.java
+++ src/main/java/ast/ExpressionNotEqual.java
@@ -1,9 +1,18 @@
 package ast;
 
+import evaluator.Evaluator;
+import evaluator.IllegalCastException;
+import objects.LuaBoolean;
+import objects.LuaObject;
 import token.TokenOperator;
 
 public class ExpressionNotEqual extends ExpressionBinary {
   ExpressionNotEqual(TokenOperator token, Expression lhs, Expression rhs) {
     super(token, lhs, rhs);
+  }
+
+  @Override
+  public LuaBoolean evaluate(Evaluator evaluator) throws IllegalCastException {
+    return new LuaBoolean(LuaBoolean.valueOf(lhs.evaluate(evaluator)) != LuaBoolean.valueOf(rhs.evaluate(evaluator)));
   }
 }
