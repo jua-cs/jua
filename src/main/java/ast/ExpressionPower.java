ec51ed52f468c2ba887a5fcafd8d87e2fcad4a53
Rémi Calixte <remicalixte.rmc@gmail.com>
2019-10-03 15:37:55 +0200

    expression: evaluate mod and pow

--- src/main/java/ast/ExpressionPower.java
+++ src/main/java/ast/ExpressionPower.java
@@ -1,9 +1,20 @@
 package ast;
 
+import evaluator.Evaluator;
+import evaluator.IllegalCastException;
+import objects.LuaNumber;
 import token.TokenOperator;
 
 public class ExpressionPower extends ExpressionBinary {
   ExpressionPower(TokenOperator token, Expression lhs, Expression rhs) {
     super(token, lhs, rhs);
+  }
+
+  @Override
+  public LuaNumber evaluate(Evaluator evaluator) throws IllegalCastException {
+    return new LuaNumber(
+        Math.pow(
+            LuaNumber.valueOf(lhs.evaluate(evaluator)).getValue(),
+            LuaNumber.valueOf(rhs.evaluate(evaluator)).getValue()));
   }
 }
