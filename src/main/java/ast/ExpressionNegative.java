e54869ca9d365163d0e743fceb16183b43bceba4
Julen Dixneuf <julen.dixneuf@student.ecp.fr>
2019-10-03 15:40:10 +0200

    implement evaluate in ExpressionNegative

--- src/main/java/ast/ExpressionNegative.java
+++ src/main/java/ast/ExpressionNegative.java
@@ -1,5 +1,9 @@
 package ast;
 
+import evaluator.Evaluator;
+import evaluator.IllegalCastException;
+import objects.LuaNumber;
+import objects.LuaObject;
 import token.Operator;
 import token.Token;
 import token.TokenFactory;
@@ -8,5 +12,11 @@ public class ExpressionNegative extends ExpressionUnary {
 
   ExpressionNegative(Token token, Expression value) {
     super(TokenFactory.create(Operator.NEGATIVE, token.getLine(), token.getPosition()), value);
+  }
+
+  @Override
+  public LuaNumber evaluate(Evaluator evaluator) throws IllegalCastException {
+    Double number = LuaNumber.valueOf(value.evaluate(evaluator)).getValue();
+    return new LuaNumber(-number);
   }
 }
