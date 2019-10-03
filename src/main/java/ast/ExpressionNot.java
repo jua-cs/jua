fd0a5df4b3a0156468ef7f49e91b31ce2f6e8c14
Rémi Calixte <remicalixte.rmc@gmail.com>
2019-10-03 15:21:47 +0200

    expression: evaluate for boolean

--- src/main/java/ast/ExpressionNot.java
+++ src/main/java/ast/ExpressionNot.java
@@ -1,5 +1,8 @@
 package ast;
 
+import evaluator.Evaluator;
+import objects.LuaBoolean;
+import objects.LuaObject;
 import token.Operator;
 import token.TokenFactory;
 import token.TokenOperator;
@@ -8,5 +11,10 @@ public class ExpressionNot extends ExpressionUnary {
 
   ExpressionNot(TokenOperator token, Expression value) {
     super(TokenFactory.create(Operator.NOT, token.getLine(), token.getPosition()), value);
+  }
+
+  @Override
+  public LuaObject evaluate(Evaluator evaluator) {
+    return new LuaBoolean(!LuaBoolean.valueOf(value.evaluate(evaluator)).getValue());
   }
 }
