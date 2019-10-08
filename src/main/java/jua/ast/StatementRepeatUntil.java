a67ca56d21b4cf9151765fc0fd1e2ff95c11ac1f
Julen Dixneuf <julen.dixneuf@student.ecp.fr>
2019-10-08 18:07:11 +0200

    add evaluator test

--- src/main/java/jua/ast/StatementRepeatUntil.java
+++ src/main/java/jua/ast/StatementRepeatUntil.java
@@ -2,19 +2,19 @@ package jua.ast;
 
 import jua.evaluator.LuaRuntimeException;
 import jua.evaluator.Scope;
-import jua.objects.LuaObject;
+import jua.objects.*;
 import jua.token.Token;
 
 
 public class StatementRepeatUntil extends Statement {
 
     private Expression condition;
-    private Statement consequence;
+    private Statement action;
 
-    public StatementRepeatUntil(Token token, Expression condition, Statement consequence) {
+    public StatementRepeatUntil(Token token, Expression condition, Statement action) {
         super(token);
         this.condition = condition;
-        this.consequence = consequence;
+        this.action = action;
     }
 
     @Override
@@ -26,25 +26,42 @@ public class StatementRepeatUntil extends Statement {
         StatementRepeatUntil that = (StatementRepeatUntil) o;
 
         if (!condition.equals(that.condition)) return false;
-        return consequence.equals(that.consequence);
+        return action.equals(that.action);
     }
 
     @Override
     public int hashCode() {
         int result = super.hashCode();
         result = 31 * result + condition.hashCode();
-        result = 31 * result + consequence.hashCode();
+        result = 31 * result + action.hashCode();
         return result;
     }
 
     @Override
     public String toString() {
         return String.format(
-                "repeat\n%s\nuntil %s", util.Util.indent(consequence.toString()), condition);
+                "repeat\n%s\nuntil %s", util.Util.indent(action.toString()), condition);
     }
 
     @Override
     public LuaObject evaluate(Scope scope) throws LuaRuntimeException {
-        return null;
+        Scope repeatScope = scope.createChild();
+
+        LuaObject ret = LuaNil.getInstance();
+
+        do{
+            ret = action.evaluate(repeatScope.createChild());
+
+            if (ret instanceof LuaReturn) {
+                return ret;
+            }
+            if (ret instanceof LuaBreak) {
+                // when we encounter a break, "unwrap" it to prevent it from propagating
+                return LuaNil.getInstance();
+            }
+        // until is !while
+        } while (!LuaBoolean.valueOf(condition.evaluate(repeatScope)).getValue());
+
+        return ret;
     }
 }
