767d210507301a9b92f17e83c54158923e9f4db4
Rémi Calixte <remicalixte.rmc@gmail.com>
2019-10-17 14:22:10 +0200

    front: use codemirror for repl

--- src/web/src/components/editor/REPL.vue
+++ src/web/src/components/editor/REPL.vue
@@ -1,24 +1,46 @@
 <template>
     <Container :reset="reset">
-        <pre class="prompt"> {{prompt}}</pre>
-        <input v-model="code" v-on:keyup.enter="run()">
+        <codemirror :options="options" @ready="onCmReady"></codemirror>
     </Container>
 </template>
 
 <script>
     import {url} from '../../util';
     import Container from './Container'
+    import {codemirror} from 'vue-codemirror'
+
 
     export default {
         name: 'REPL',
         components: {
+            codemirror,
             Container
         },
         data() {
             return {
-                code: "",
-                prompt: "",
+                cminstance: null,
                 ws: null,
+                options: {
+                    electricChars: false,
+                    theme: 'mdn-like',
+                    mode: 'lua',
+                    smartIndent: false,
+                    lineWrapping: true,
+                    gutters: ["CodeMirror-linenumber", "prompt"],
+                    extraKeys: {
+                        "Up": this.up,
+                        "Down": this.down,
+                        "Enter": this.enter,
+                        "Shift-Enter": this.enter,
+                    },
+                    indentUnit: 4,
+                    undoDepth: 1,
+                    autofocus: true
+                },
+                history: [],
+                historyIndex: 0,
+                text: "",
+                line: 0,
             }
         },
         created() {
@@ -28,30 +50,119 @@
             this.ws.close();
         },
         methods: {
-            run: function () {
-                this.ws.send(this.code + '\n');
-                this.prompt += this.code + '\n';
-                this.code = "";
+            onCmReady: function (cm) {
+                this.cminstance = cm;
             },
-            connect: function() {
+            setLine: function (line, text) {
+                if (line >= 0 && line < this.cminstance.doc.size) {
+                    this.cminstance.replaceRange(text, {line: line, ch: 0}, {
+                        line: line,
+                        ch: this.cminstance.getLine(line).length
+                    });
+                }
+            },
+            setMarker: function (line, text) {
+                let element = document.createElement("p");
+                element.innerText = text;
+                this.cminstance.setGutterMarker(line, "CodeMirror-linenumber", element);
+            },
+            incrementLine: function () {
+                this.cminstance.markText({line: 0, ch: 0}, {
+                    line: this.line + 1,
+                    ch: 0
+                }, {readOnly: true});
+                this.line++;
+            },
+            connect: function () {
                 this.ws = new WebSocket(`ws://${url}/api/v1/repl`);
-                this.ws.onmessage = this.updatePrompt;
+                this.ws.onmessage = this.onMessage;
             },
-            reset: function() {
+            reset: function () {
                 this.ws.close();
-                this.code = "";
-                this.prompt = "";
                 this.connect();
             },
-            updatePrompt: function (message) {
+            onMessage: function (message) {
                 let payload = JSON.parse(message.data);
                 if (typeof payload.return !== "undefined") {
-                    this.prompt += payload.return;
+                    if (payload.return === "> ") {
+                        this.setMarker(this.line, ">>>");
+                    } else {
+                        this.print(payload.return);
+                    }
                 }
                 if (typeof payload.error !== "undefined") {
-                    this.prompt += payload.error;
+                    this.print(payload.error, "redText");
                 }
-            }
+            },
+            up: function () {
+                if (this.historyIndex <= 0) {
+                    this.historyIndex = 0;
+                    return;
+                }
+                //If this is a new line, store it into the text buffer
+                if (this.historyIndex === this.history.length) {
+                    this.text = this.cminstance.getLine(this.line);
+                }
+                //Display an earlier line
+                this.historyIndex--;
+                this.setLine(this.line, this.history[this.historyIndex]);
+            },
+
+            down: function () {
+                if (this.historyIndex >= this.history.length) {
+                    return;
+                }
+                this.historyIndex++;
+
+                //Display the line in the text buffer if this is the end of history...
+                let text = this.text;
+                //or the requested line in history
+                if (this.historyIndex < this.history.length) {
+                    text = this.history[this.historyIndex];
+                }
+                this.setLine(this.line, text);
+            },
+            enter: function () {
+                let text = this.cminstance.getLine(this.line);
+
+                this.historyIndex = this.history.push(text);
+                this.setLine(this.line, text + '\n');
+                this.incrementLine();
+                this.ws.send(text + '\n');
+            },
+
+            print: function (message, className) {
+                let ln = this.line;
+
+                message = String(message);
+                let text = this.cminstance.getLine(this.line);
+                message = message.replace(/\n/g, '\r') + '\n';
+
+                let cursor = 0;
+                if (text) {
+                    this.setMarker(this.line, "");
+                    cursor = this.cminstance.getCursor().ch;
+                }
+
+                this.setLine(this.line, message);
+                this.incrementLine();
+                if (className) {
+                    this.cminstance.markText({line: ln, ch: 0}, {line: ln, ch: message.length}, {className});
+                }
+
+                if (text) {
+                    this.setLine(this.line, text);
+                    this.cminstance.setCursor({line: this.line, ch: cursor});
+                }
+            },
+
         }
     }
 </script>
+
+
+<style>
+    .redText {
+        color: red;
+    }
+</style>
