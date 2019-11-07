f432628542ec1dd28ff6d3c741a51fa659d14dc1
Julen Dixneuf <julen.dixneuf@student.ecp.fr>
2019-11-07 18:37:41 +0100

    add bitwise operation to evaluator

--- src/main/java/jua/ast/ExpressionBitwiseAnd.java
+++ src/main/java/jua/ast/ExpressionBitwiseAnd.java
@@ -2,6 +2,7 @@ package jua.ast;
 
 import jua.evaluator.LuaRuntimeException;
 import jua.evaluator.Scope;
+import jua.objects.LuaNumber;
 import jua.objects.LuaObject;
 import jua.token.TokenOperator;
 
@@ -13,6 +14,10 @@ public class ExpressionBitwiseAnd extends ExpressionBinary {
 
   @Override
   public LuaObject evaluate(Scope scope) throws LuaRuntimeException {
-    return null;
+    // We cast Double to Long as java doesn't support bitwise operations on floating numbers
+    Long number =
+            LuaNumber.valueOf(lhs.evaluate(scope)).getValue().longValue()
+                    & LuaNumber.valueOf(rhs.evaluate(scope)).getValue().longValue();
+    return new LuaNumber(number.doubleValue());
   }
 }
---
+++ src/test/java/jua/evaluator/testdata/bitwise.lua
@@ -0,0 +1,6 @@
+print(1 & 0)
+print(1 | 0)
+print(1 ~ 1)
+print(~ 1)
+print(1 >> 1)
+print(1 << 1)
